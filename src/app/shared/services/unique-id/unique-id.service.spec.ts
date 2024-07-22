b484db81d45a14b856f5ceb6fb2c8eec7e035943
Andrietti <meier.gabriel@hotmail.com>
2024-07-22 16:30:45 -0300

    atts

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,26 +1,10 @@
+import { UniqueIdService } from "./unique-id.service";
+
+
 describe('O artefato que queremos testar', () => {
-    it('generateUniqueIdWithPrefix', () => {
-      // Arrange
-      // Preparação do cenário de teste
-      // Criação de objetos, mocks, spies, etc.
-  
-      // Act
-      // Execução do cenário de teste
-      // Chamada do método que queremos testar
-  
-      // Assert
-      // Verificação do resultado do cenário de teste
-      // Verificação de comportamento, estado, etc.
-    })
-  
-    it('getNumberOfGeneratedUniqueIds', () => {
-      // Arrange
-      // ...
-  
-      // Act
-      // ...
-  
-      // Assert
-      // ...
+    it('generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+      const service = new UniqueIdService();
+      const id = service.generateUniqueIdWithPrefix('app');
+      expect(id).toContain('app-');
     })
 })
